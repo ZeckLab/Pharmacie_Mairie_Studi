f1275d1988fec3bd83da3a332acdc6d51acf6a5b
adeline64 <adeline.lacabanne@gmail.com>
2023-07-16 16:57:09 +0200

    update

--- src/app/components/home/brands-home/brands-home.component.ts
+++ src/app/components/home/brands-home/brands-home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SharedJsonService} from "../../../sercices/shared-json/shared-json.service";
 
 @Component({
   selector: 'app-brands-home',
   templateUrl: './brands-home.component.html',
   styleUrls: ['./brands-home.component.scss']
 })
-export class BrandsHomeComponent {
+export class BrandsHomeComponent implements OnInit {
 
+  jsonData: any;
+
+  constructor(private sharedJsonService: SharedJsonService) {
+  }
+
+  ngOnInit() {
+    this.sharedJsonService.getData().subscribe(data => {
+      this.jsonData = data;
+      // Show only the first 6 elements of the "brands" array
+      this.jsonData.brands = this.jsonData.brands.slice(0, 6);
+    });
+
+  }
 }
