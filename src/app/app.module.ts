32b0e5e86e1fa1867be9bde854d14b3ca327dd6f
adeline64 <adeline.lacabanne@gmail.com>
2023-07-16 15:48:58 +0200

    creation brands in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrandsHomeComponent } from './components/home/brands-home/brands-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BrandsHomeComponent
   ],
   imports: [
     BrowserModule,
