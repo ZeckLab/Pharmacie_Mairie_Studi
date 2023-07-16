528954e606928735b8af831a97806806cc7a4528
adeline64 <adeline.lacabanne@gmail.com>
2023-07-16 12:19:01 +0200

    The hours-localisation is finish. HTML and SCSS is ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LocalisationComponent } from './components/home/localisation/localisation.component';
+import { HoursLocalisationComponent } from './components/home/localisation/hours-localisation/hours-localisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LocalisationComponent
+    LocalisationComponent,
+    HoursLocalisationComponent
   ],
   imports: [
     BrowserModule,
