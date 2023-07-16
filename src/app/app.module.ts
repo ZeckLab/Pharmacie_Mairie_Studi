46b0f4fea45fc66d209b60b60e6ece239cf2d114
adeline64 <adeline.lacabanne@gmail.com>
2023-07-16 03:14:40 +0200

    first commit in this branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LocalisationComponent } from './components/home/localisation/localisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LocalisationComponent
   ],
   imports: [
     BrowserModule,
