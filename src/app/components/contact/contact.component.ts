71d8c23f4b46f66540e6cbffd0b7b70dfe74c248
adeline64 <adeline.lacabanne@gmail.com>
2023-07-17 23:58:38 +0200

    update but not sure

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  selectedCountryCode: string = '';
+  formData = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    message: ''
+  };
 
 }
