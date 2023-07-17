335ff700c3707055e20f7fc2db0af975371138d9
adeline64 <adeline.lacabanne@gmail.com>
2023-07-17 13:40:36 +0200

    begin of event in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { EventComponent } from './components/home/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
