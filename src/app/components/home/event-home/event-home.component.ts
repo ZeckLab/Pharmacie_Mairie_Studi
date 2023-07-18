edbca4fe63680e98bbc94f44931b52cb3c309378
ZeckLab <sturbout@gmail.com>
2023-07-19 00:40:10 +0200

    fix : map display bug. Add date of event. Add provider fr for the date

--- src/app/components/home/event-home/event-home.component.ts
+++ src/app/components/home/event-home/event-home.component.ts
@@ -16,6 +16,7 @@ export class EventHomeComponent implements OnInit {
         this.sharedJsonService.getData().subscribe(data => {
         // Assuming the JSON data contains an "event" property with an array of events
             this.event = data.event;
+            this.event.date = new Date(this.event.date.year, this.event.date.month - 1, this.event.date.day);
         });
     }
 
