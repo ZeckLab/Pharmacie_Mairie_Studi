71c501b0a1d760d5b6d0e5a9f30eeb39897318fb
ZeckLab <sturbout@gmail.com>
2023-07-18 17:12:16 +0200

    Fix : display images. Design : update of design of Products waiting the slideCOmponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { EventComponent } from './components/home/event-home/event.component';
-import { BrandsHomeComponent } from './components/home/brands-home/brands-home.component';
 import { ProductsHomeComponent } from './components/home/products-home/products-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EventComponent,
-    BrandsHomeComponent,
     ProductsHomeComponent
   ],
   imports: [
